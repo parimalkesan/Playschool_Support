3f2f7899c4d65d7d031c4e062bccd8664f1d57eb
ParimalKesan <parimalkesan3@gmail.com>
2020-06-12 23:39:21 +0530

    The mainactivity displays splashscreen while loginactivity is used for userlogin.The bottomnavigationactivity is used to display bottomnavigationview

--- app/src/main/java/Fragments/ProfileFragment.java
+++ app/src/main/java/Fragments/ProfileFragment.java
@@ -2,20 +2,67 @@ package Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.playschool_support.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.auth.User;
 
+import Model.Users;
+import de.hdodenhof.circleimageview.CircleImageView;
+//to display profile of current user
 public class ProfileFragment extends Fragment {
+
+    CircleImageView prof_image_view;
+    TextView prof_email;
+
+    FirebaseUser firebaseUser;
+    FirebaseFirestore firebaseFirestore;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        //get reference to prof_image_view and prof_email_tv
+        prof_image_view = view.findViewById(R.id.prof_image_view);
+        prof_email = view.findViewById(R.id.prof_email_tv);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        firebaseFirestore=FirebaseFirestore.getInstance();
+
+        //set email and image for current user
+        DocumentReference docRef = firebaseFirestore.collection("Users").document(firebaseUser.getUid());
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Users user = documentSnapshot.toObject(Users.class);
+                prof_email.setText(user.getEmail());
+                if ("default".equals(user.getImageUrl())) {
+                    prof_image_view.setImageResource(R.mipmap.ic_launcher);
+                } else {
+                        Glide.with(getContext()).load(user.getImageUrl()).into(prof_image_view);
+                }
+            }
+        });
+        return view;
     }
 }
